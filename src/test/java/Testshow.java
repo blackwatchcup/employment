5943d1e25febfd2f9f7ac616472bcf70e742d24c
zhangzhiwei <zhangzhiweichange@gmail.com>
2016-07-07 16:51:18 +0800

    first:

--- src/test/java/Testshow.java
+++ src/test/java/Testshow.java
@@ -1,27 +1,40 @@
 
+import java.util.Date;
+
 import javax.annotation.Resource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.etc.employment.entity.Member;
 import com.etc.employment.entity.User;
 import com.etc.employment.service.IUserService;
+import com.etc.employment.service.MemberService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"classpath*:config/applicationContext.xml", "classpath*:config/dispatcher-servlet.xml"})
 public class Testshow {
+//	@Resource
+//	private IUserService userService;
+//	@Test
+//	public void testConn() {
+//		System.out.println("aaaaaaaaaaaaaa");
+//	}
+//	@Test
+//	public void addUser() {
+//		User user = new User("yakov", "123");
+//		userService.addUser(user);
+//		System.out.println("添加成功。。。");
+//	}
 	@Resource
-	private IUserService userService;
+	private MemberService memberService;
 	@Test
-	public void testConn() {
-		System.out.println("aaaaaaaaaaaaaa");
-	}
-	@Test
-	public void addUser() {
-		User user = new User("yakov", "123");
-		userService.addUser(user);
-		System.out.println("添加成功。。。");
+	public void add() {
+		Date date = new Date();
+		Member m1 = new Member(3, "小谍", "123", "12345687", "145021225@qq.com", "小巫婆", date, "1","dfd");
+		memberService.addMember(m1);
 	}
 }
